1c01e0fb8e9266a4c3680c2f1566e8db12bb60ed
Kishore <112462625+Anonymous12k@users.noreply.github.com>
2025-03-21 12:57:31 +0530

    attrition_app.py

--- attrition_app.py
+++ attrition_app.py
@@ -18,7 +18,7 @@ st.set_page_config(page_title="Employee Attrition Predictor & Visuals", page_ico
 def add_banner():
     st.markdown("""
         <div style="background-color:#2c3e50;padding:15px;border-radius:10px;margin-bottom:20px;">
-        <h1 style="color:white;text-align:center;">🏢 ACME Corp - Employee Attrition Predictor Dashboard</h1>
+        <h1 style="color:white;text-align:center;">🏢 SIMATS Engineering - Employee Attrition Predictor Dashboard</h1>
         </div>
         """, unsafe_allow_html=True)
 
@@ -91,4 +91,4 @@ sns.heatmap(data.corr(), annot=False, cmap="coolwarm", ax=ax3)
 ax3.set_title("Correlation Between Features")
 st.pyplot(fig3)
 
-st.sidebar.write("Developed by: L KISHORE | ACME Corp")
+st.sidebar.write("Developed by: L KISHORE | SIMATS Engineering")
